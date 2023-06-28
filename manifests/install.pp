8ef885bc673633c310a44ead39e8937494bfe672
Seth Tunstall <seth.tunstall@byhiras.com>
2023-06-28 15:55:28 +0000

    refactoring WIP

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,6 @@ $package = undef # from hiera instead of old-school case() stupidity
 ) inherits ipaclient::params {
   package { $package:
     ensure          => installed,
-    install_options => $package_options,
   }
 
 }
